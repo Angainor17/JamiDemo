973e6d9029382fc55e5c9969ea1222dceb3c06df
angai <angainor17@gmail.com>
2020-04-20 03:12:49 +0300

    p2plib

--- app/src/main/java/com/voronin/app/MainActivity.kt
+++ app/src/main/java/com/voronin/app/MainActivity.kt
@@ -1,52 +1,148 @@
 package com.voronin.app
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import io.libp2p.core.Host
+import io.libp2p.core.*
 import io.libp2p.core.dsl.HostBuilder
 import io.libp2p.core.multiformats.Multiaddr
 import io.libp2p.protocol.Ping
 import io.libp2p.protocol.PingController
 import kotlinx.android.synthetic.main.activity_main.*
-
+import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
 
-    val handler = Handler()
-
-    val node: Host = HostBuilder()
+    var host: Host = HostBuilder()
         .protocol(Ping())
-        .listen("/ip4/127.0.0.1/tcp/0")
+        .listen(
+            "/ip4/127.0.0.1/tcp/0"
+        )
         .build()
 
-    private val apiTest = Runnable {
-        // start listening
-        node.start().get()
+    init {
+        host.addConnectionHandler(object : ConnectionHandler {
+            override fun handleConnection(conn: Connection) {
+                logMessage("handleConnection localAddress: " + conn.localAddress())
+                logMessage("handleConnection remoteAddress: " + conn.remoteAddress())
+            }
+        })
 
-        val multiaddrs = (node.listenAddresses())
-        multiaddrs.forEachIndexed{ index, value ->
-            Log.d("voronin", "Address $index = $value")
-        }
-
-        val address = Multiaddr.fromString(multiaddrs[0].toString())
-        val pinger : PingController = Ping().dial(node, address).controller.get()
-//        /ip4/127.0.0.1/tcp/32912/ipfs/QmNMaqmmFbwiXgwfrpt55aq8FYSo5HxDJZNE6zrnLrSN3z
-        Log.d("voronin", "Sending 5 ping messages to $address")
-        for (i in 1..5) {
-            val latency = pinger.ping().get()
-            Log.d("voronin", "Ping " + i + ", latency " + latency + "ms")
-        }
-        node.stop().get()
+        host.addStreamHandler(object : StreamHandler<PingController?> {
+            override fun handleStream(stream: Stream): CompletableFuture<PingController> {
+                logMessage("handleStream remotePeerId =" + stream.remotePeerId())
+                logMessage("handleStream localAddress= " + stream.connection.localAddress())
+                logMessage("handleStream remoteAddress= " + stream.connection.remoteAddress())
+                logMessage("--------------------------------------------------")
+                return CompletableFuture()
+            }
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonLoad.setOnClickListener {
-            handler.post(apiTest)
+        buttonPing.setOnClickListener { ping() }
+        buttonStart.setOnClickListener { start() }
+        buttonStop.setOnClickListener { stop() }
+        buttonRefreshPeerId.setOnClickListener { refreshPeerId() }
+        buttonListenTo.setOnClickListener { listenToRefresh() }
+        buttonClearLog.setOnClickListener { clearLog() }
+
+        registerForContextMenu(peerId)
+    }
+
+    private fun clearLog() {
+        logs.text = ""
+    }
+
+    private fun listenToRefresh() {
+        val listenToAddress = listenToToEditText.text.toString()
+        if (listenToAddress.isNotEmpty()) {
+            try {
+                host = HostBuilder()
+                    .protocol(Ping())
+                    .listen(
+                        "/ip4/127.0.0.1/tcp/0",
+                        listenToAddress
+                    )
+                    .build()
+                start()
+                logMessage("Listen to $listenToAddress SUCCEED!!!")
+            } catch (e: Exception) {
+                logMessage("Listen to $listenToAddress FAILED!!!")
+                host = HostBuilder()
+                    .protocol(Ping())
+                    .listen("/ip4/127.0.0.1/tcp/0")
+                    .build()
+                start()
+            }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stop()
+    }
+
+    private fun start() {
+        host.start().get()
+    }
+
+    private fun stop() {
+        host.stop().get()
+    }
+
+    private fun refreshPeerId() {
+        peerId.text = host.peerId.toBase58()
+    }
+
+    private fun ping() {
+        val userPeerTo = peerToEditText.text.toString()
+        if (userPeerTo.isNotEmpty()) {
+            try {
+                pingTo(Multiaddr.fromString(userPeerTo))
+                val connection = host.network.connect(Multiaddr.fromString(userPeerTo)).get()
+                if (connection != null) {
+                    logMessage("Connection to $userPeerTo SUCCEED!")
+                }
+
+            } catch (e: Exception) {
+                logMessage("Connection to $userPeerTo FAIL!")
+            }
+            return
+        }
+
+        val multiaddrs = host.listenAddresses()
+        multiaddrs.forEachIndexed { index, value ->
+            logMessage("Address $index = $value")
+        }
+
+        multiaddrs.map { Multiaddr.fromString(it.toString()) }.forEach { address ->
+            pingTo(address)
+        }
+    }
+
+    private fun pingTo(address: Multiaddr) {
+        try {
+            val pinger: PingController = Ping().dial(host, address).controller.get()
+
+            logMessage("Sending 5 ping messages to $address")
+            for (i in 1..5) {
+                val latency = pinger.ping().get()
+                logMessage("Ping " + i + ", latency " + latency + "ms")
+            }
+        } catch (e: Exception) {
+            logMessage("Sending 5 ping messages to $address - FAIL!!! ")
+        }
+    }
+
+    private fun logMessage(text: String) {
+        Log.d("voroninDebug", text)
+        val oldText = logs.text.toString()
+        val newText = "" + (if (oldText.isEmpty()) "" else "\n") + text
+
+        logs.text = oldText + newText
     }
 }
